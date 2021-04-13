1ce70840725ed151f78c05cd5bea4b08554d9141
Brian Taylor <taylor.brian99@yahoo.com>
2021-04-13 02:23:05 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+ const names = ["Ada", "Brendan", "Ali"]
+function writeCards(names, event) { 
+    let greeting = [] 
+    for (let i = 0; i < names.length; i++) {
+        greeting.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    } 
+    return greeting
+     
+}
+
+function countDown(startingNumber) {
+    while (startingNumber > 0) {
+        console.log(startingNumber) 
+        startingNumber -= 1
+    } 
+    console.log(startingNumber)
+}
+    
+ 
